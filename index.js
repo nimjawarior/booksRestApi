4b6fae9fffa4fd01d40b62735624ac567ec79c9b
eid.luigi <eid.luigi@courrier.uqam.ca>
2022-07-08 14:42:52 -0400

    V1 of code

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 require("dotenv").config();
+const booksRoute = require("./routes/books");
 
 const PORT = process.env.PORT || 3000;
 
+//MiddleWear
+app.use(express.json()); // you can expect that we send JSON in the url
+app.use(express.urlencoded({ extended: true })); //We can also expect to send object, array, array of objects
+
+//routes
+app.use("/api/books", booksRoute);
 //Connect to mongodb atlas
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
